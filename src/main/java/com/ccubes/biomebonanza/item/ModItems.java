60127e9519241a3c64437d8bf3e52de095b11247
Cyo <117950417+CyoNearYou@users.noreply.github.com>
2023-11-23 18:49:02 -0500

    Create ModItems.java

---
+++ src/main/java/com/ccubes/biomebonanza/item/ModItems.java
@@ -0,0 +1,9 @@
+package com.ccubes.biomebonanza.item;
+
+import com.ccubes.biomebonanza.BiomeBonanza;
+
+public class ModItems {
+  public static void registerModItems() {
+    BiomeBonanza.LOGGER.info("Registering a bonanza of mod items! (" + BiomeBonanza.MOD_ID + ")")
+  }
+}
