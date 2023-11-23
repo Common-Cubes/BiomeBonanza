54e33fd1550af94a27489c1b0c0e29ca1f534e25
Cyo <117950417+CyoNearYou@users.noreply.github.com>
2023-11-23 12:25:00 -0500

    Create BiomeBonanzaClient.java

---
+++ src/main/java/com/ccubes/biomebonanza/BiomeBonanzaClient.java
@@ -0,0 +1,10 @@
+package com.ccubes.biomebonanza;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BiomeBonanzaClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+
+  }
+}
